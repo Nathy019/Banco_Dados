2b2dfc0e36f16c46dd27739e4eadf48360b18794
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2023-04-20 21:18:22 -0300

    Adicionar arquivos de projeto.

---
+++ Banco_Dados/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Banco_Dados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPesquisaBD = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PESQUISA";
+            // 
+            // txtPesquisaBD
+            // 
+            this.txtPesquisaBD.BackColor = System.Drawing.Color.PaleGreen;
+            this.txtPesquisaBD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPesquisaBD.Location = new System.Drawing.Point(131, 34);
+            this.txtPesquisaBD.Name = "txtPesquisaBD";
+            this.txtPesquisaBD.Size = new System.Drawing.Size(195, 20);
+            this.txtPesquisaBD.TabIndex = 1;
+            this.txtPesquisaBD.TextChanged += new System.EventHandler(this.txtPesquisaBD_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(395, 199);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Romantic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button1.Location = new System.Drawing.Point(261, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculo de Novos Dados";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(454, 371);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtPesquisaBD);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPesquisaBD;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Banco_Dados/Form1.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco_Dados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=viagens;password=;");
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM t_veiculos", conn);
+            //datatable  = armazena os resultados da consulta
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            //MySqlDataAdapter= preencher o datatable com os resultados da consulta
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            conn.Close();
+
+            //vincule  data ao datagrid para exibir os dados na grade
+            dataGridView1.DataSource= dt;
+        }
+
+        private void txtPesquisaBD_TextChanged(object sender, EventArgs e)
+        {
+            string pesquisa = txtPesquisaBD.Text;
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=viagens;password=;");
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM t_veiculos where modelo like '%"+pesquisa+"%'",conn);
+            //datatable  = armazena os resultados da consulta
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            //MySqlDataAdapter= preencher o datatable com os resultados da consulta
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            conn.Close();
+
+            //vincule  data ao datagrid para exibir os dados na grade
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
